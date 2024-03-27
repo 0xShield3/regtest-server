a9195bb1e3e8b7f6ab56f6b06b30f04a5649afe0
Isaac Patka <ipatka@gmail.com>
2024-03-27 18:00:19 -0400

    feat: Background mining
    mine blocks every 5s in bg

--- docker/run_bitcoind_service.sh
+++ docker/run_bitcoind_service.sh
@@ -6,3 +6,14 @@ sleep 2
 /usr/bin/bitcoin-cli -regtest createwallet default
 ADDRESS=$(/usr/bin/bitcoin-cli -regtest getnewaddress "" bech32)
 /usr/bin/bitcoin-cli -regtest generatetoaddress 432 $ADDRESS
+
+# Start mining in the background
+(
+    while true; do
+        /usr/bin/bitcoin-cli -regtest generatetoaddress 1 $ADDRESS
+        sleep 5
+    done
+) &
+
+# Disown the background mining process so it doesn't terminate when the script ends
+disown
